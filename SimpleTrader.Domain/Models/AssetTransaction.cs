1c80747a0d6f21e28287114b61614db57fccaa6e
Ranjan Aggarwal <ranjanaggrawal@gmail.com>
2022-10-05 21:12:39 +0530

    Add project files.

---
+++ SimpleTrader.Domain/Models/AssetTransaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Models
+{
+    public class AssetTransaction
+    {
+        public int Id { get; set; }
+        public string AccountHolder { get; set; }
+        public double Balance { get; set; }
+    }
+}
---
+++ SimpleTrader.Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Status { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
